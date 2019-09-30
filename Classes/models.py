b052f251b764adb683f706c0f6f5aad6faab2278
Navid Rayhan <rayhann@mcmaster.ca>
2019-09-30 19:09:22 -0400

    making models

--- Classes/models.py
+++ Classes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    course_code = models.CharField(max_length=10)
+    institution = models.CharField(default="McMaster University", max_length= 50)
+    tutorial_1_start = models.DateTimeField(blank=True, null=True)
+    tutorial_1_end = models.DateTimeField(blank=True, null=True)
+    tutorial_2_start = models.DateTimeField(blank=True, null = True)
+    tutorial_2_end = models.DateTimeField(blank=True,null=True)
+    lecture_1_start = models.DateTimeField(blank=True,null=True)
+    lecture_1_end = models.DateTimeField(blank=True, null=True)
+    lecture_2_start = models.DateTimeField(blank=True,null=True)
+    lecture_2_end = models.DateTimeField(blank=True, null=True)
+    lecture_3_start = models.DateTimeField(blank=True,null=True)
+    lecture_3_end = models.DateTimeField(blank=True, null=True)
+    midterm1 = models.DateTimeField(blank=True, null=True)
+    midterm2 = models.DateTimeField(blank=True,null=True)
+    final_exam = models.DateTimeField(blank=True,null=True)
+
+
+    
--- Tasks/models.py
+++ Tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from Classes.models import Course
 # Create your models here.
+class Exam(models.Model):
+    name = models.CharField(max_length=50)
+    course = models.ManyToManyField(Course)
+    worth = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    notes = models.TextField()
+
+class CodingChallenge(models.Model):
+    company = models.CharField(max_length=50)
+    due_by = models.DateTimeField()
+    completed_status = models.BooleanField(default=False)
+    allowed_time = models.DateTimeField()
+    notes = models.TextField()
+
+class Assignment(models.Model):
+    course = models.OneToOneField(Course, on_delete= models.PROTECT, blank=True,null=True)
+    due_by = models.DateTimeField()
+
+class Other(models.Model):
+    due_by = models.DateTimeField()
+    notes = models.TextField()
